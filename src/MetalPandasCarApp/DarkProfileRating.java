7f79382fe71f32db0b3a0c59d7694cf7ea73af1a
Rebbica007 <jrgrunewald5184@eagle.fgcu.edu>
2019-11-17 23:07:39 -0500

    Rating page fixed!

--- src/MetalPandasCarApp/DarkProfileRating.java
+++ src/MetalPandasCarApp/DarkProfileRating.java
@@ -13,7 +13,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import org.controlsfx.control.Rating;
 
@@ -24,15 +28,16 @@ public class DarkProfileRating {
   @FXML public Label ratingLabelTitle;
   @FXML public Label ratingLabel;
   @FXML public Button confirmRating;
+  @FXML public Circle rating1;
+  @FXML public Arc rating2;
+  @FXML public Circle rating3;
+  @FXML public Arc rating4;
+  @FXML public Circle rating5;
 
   public void initialize(URL arg0, ResourceBundle arg1){
     try{
-      starRating.ratingProperty().addListener(new ChangeListener<Number>() {
-        @Override
-        public void changed(ObservableValue<? extends Number> arg0, Number t, Number t1) {
-          ratingLabel.setText("Rating: " + t1.toString());
-        }
-      });
+      starRating.ratingProperty().addListener(
+          (arg01, t, t1) -> ratingLabel.setText("Rating: " + t1.toString()));
     } catch (java.lang.NullPointerException exception) {
       exception.printStackTrace();
     }
@@ -45,4 +50,44 @@ public class DarkProfileRating {
     homeStage.setScene(homePageScene);
     homeStage.show();
   }
+
+    public void handleRating1Action(MouseEvent mouseEvent) {
+      rating1.setFill(javafx.scene.paint.Color.YELLOW);
+      rating2.setFill(Color.TRANSPARENT);
+      rating3.setFill(Color.TRANSPARENT);
+      rating4.setFill(Color.TRANSPARENT);
+      rating5.setFill(Color.TRANSPARENT);
+    }
+
+    public void handleRating2Action(MouseEvent mouseEvent) {
+      rating1.setFill(javafx.scene.paint.Color.YELLOW);
+      rating2.setFill(javafx.scene.paint.Color.YELLOW);
+      rating3.setFill(Color.TRANSPARENT);
+      rating4.setFill(Color.TRANSPARENT);
+      rating5.setFill(Color.TRANSPARENT);
+    }
+
+    public void handleRating3Action(MouseEvent mouseEvent) {
+      rating1.setFill(javafx.scene.paint.Color.YELLOW);
+      rating2.setFill(javafx.scene.paint.Color.YELLOW);
+      rating3.setFill(javafx.scene.paint.Color.YELLOW);
+      rating4.setFill(Color.TRANSPARENT);
+      rating5.setFill(Color.TRANSPARENT);
+    }
+
+    public void handleRating4Action(MouseEvent mouseEvent) {
+      rating1.setFill(javafx.scene.paint.Color.YELLOW);
+      rating2.setFill(javafx.scene.paint.Color.YELLOW);
+      rating3.setFill(javafx.scene.paint.Color.YELLOW);
+      rating4.setFill(javafx.scene.paint.Color.YELLOW);
+      rating5.setFill(Color.TRANSPARENT);
+    }
+
+    public void handleRating5Action(MouseEvent mouseEvent) {
+      rating1.setFill(javafx.scene.paint.Color.YELLOW);
+      rating2.setFill(javafx.scene.paint.Color.YELLOW);
+      rating3.setFill(javafx.scene.paint.Color.YELLOW);
+      rating4.setFill(javafx.scene.paint.Color.YELLOW);
+      rating5.setFill(javafx.scene.paint.Color.YELLOW);
+    }
 }
--- src/MetalPandasCarApp/LightProfileRating.java
+++ src/MetalPandasCarApp/LightProfileRating.java
@@ -13,7 +13,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import org.controlsfx.control.Rating;
 
@@ -24,25 +28,66 @@ public class LightProfileRating {
   @FXML public Label ratingLabelTitle;
   @FXML public Label ratingLabel;
   @FXML public Button confirmRating;
+  @FXML public Circle rating1;
+  @FXML public Arc rating2;
+  @FXML public Circle rating3;
+  @FXML public Arc rating4;
+  @FXML public Circle rating5;
 
   public void initialize(URL arg0, ResourceBundle arg1){
     try{
-      starRating.ratingProperty().addListener(new ChangeListener<Number>() {
-        @Override
-        public void changed(ObservableValue<? extends Number> arg0, Number t, Number t1) {
-          ratingLabel.setText("Rating: " + t1.toString());
-        }
-      });
+      starRating.ratingProperty().addListener(
+          (arg01, t, t1) -> ratingLabel.setText("Rating: " + t1.toString()));
     } catch (java.lang.NullPointerException exception) {
       exception.printStackTrace();
     }
   }
 
   public void handleRatingAction(ActionEvent actionEvent) throws IOException {
-    Parent homePageParent = FXMLLoader.load(getClass().getResource("lightAppointments.fxml"));
+    Parent homePageParent = FXMLLoader.load(getClass().getResource("darkAppointments.fxml"));
     Scene homePageScene = new Scene(homePageParent);
     Stage homeStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
     homeStage.setScene(homePageScene);
     homeStage.show();
+  }
+
+  public void handleRating1Action(MouseEvent mouseEvent) {
+    rating1.setFill(javafx.scene.paint.Color.YELLOW);
+    rating2.setFill(Color.TRANSPARENT);
+    rating3.setFill(Color.TRANSPARENT);
+    rating4.setFill(Color.TRANSPARENT);
+    rating5.setFill(Color.TRANSPARENT);
+  }
+
+  public void handleRating2Action(MouseEvent mouseEvent) {
+    rating1.setFill(javafx.scene.paint.Color.YELLOW);
+    rating2.setFill(javafx.scene.paint.Color.YELLOW);
+    rating3.setFill(Color.TRANSPARENT);
+    rating4.setFill(Color.TRANSPARENT);
+    rating5.setFill(Color.TRANSPARENT);
+  }
+
+  public void handleRating3Action(MouseEvent mouseEvent) {
+    rating1.setFill(javafx.scene.paint.Color.YELLOW);
+    rating2.setFill(javafx.scene.paint.Color.YELLOW);
+    rating3.setFill(javafx.scene.paint.Color.YELLOW);
+    rating4.setFill(Color.TRANSPARENT);
+    rating5.setFill(Color.TRANSPARENT);
+  }
+
+  public void handleRating4Action(MouseEvent mouseEvent) {
+    rating1.setFill(javafx.scene.paint.Color.YELLOW);
+    rating2.setFill(javafx.scene.paint.Color.YELLOW);
+    rating3.setFill(javafx.scene.paint.Color.YELLOW);
+    rating4.setFill(javafx.scene.paint.Color.YELLOW);
+    rating5.setFill(Color.TRANSPARENT);
+  }
+
+  public void handleRating5Action(MouseEvent mouseEvent) {
+    rating1.setFill(javafx.scene.paint.Color.YELLOW);
+    rating2.setFill(javafx.scene.paint.Color.YELLOW);
+    rating3.setFill(javafx.scene.paint.Color.YELLOW);
+    rating4.setFill(javafx.scene.paint.Color.YELLOW);
+    rating5.setFill(javafx.scene.paint.Color.YELLOW);
   }
 }
