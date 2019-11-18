11a2089455ee73f84cbe484b4259b16536ee41e4
owenr <darthroxas456@gmail.com>
2019-11-18 14:51:15 -0500

    edited forgot and resetpassword pages

--- src/MetalPandasCarApp/PandaReset.java
+++ src/MetalPandasCarApp/PandaReset.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,12 +20,31 @@ public class PandaReset {
   @FXML public PasswordField password;
   @FXML public PasswordField reenterPassword;
   @FXML public Text resetPasswordLabel;
+  @FXML public Text passwordCheckText;
 
-  public void handleConfirmAction(ActionEvent actionEvent) throws IOException {
-    Parent homePageParent = FXMLLoader.load(getClass().getResource("pandaLogin.fxml"));
-    Scene homePageScene = new Scene(homePageParent);
-    Stage homeStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-    homeStage.setScene(homePageScene);
-    homeStage.show();
+  public void handleConfirmAction(ActionEvent actionEvent) throws IOException{
+    if (password.getText().equals("")){
+      passwordCheckText.setText("please enter a new Password");
+    }
+    else if (password.getText().equals(reenterPassword.getText())) {
+      Parent homePageParent = FXMLLoader.load(getClass().getResource("pandaLogin.fxml"));
+      Scene homePageScene = new Scene(homePageParent);
+      Stage homeStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+      homeStage.setScene(homePageScene);
+      homeStage.show();
+    }
+    else if(!password.getText().equals(reenterPassword.getText())){
+      passwordCheckText.setText("Passwords do not match.");
+    }
+  }
+
+  public void handlePopupAction(MouseEvent mouseEvent) throws IOException {
+    if (password.getText().equals(reenterPassword.getText())) {
+      Parent homePageParent = FXMLLoader.load(getClass().getResource("pandaResetPasswordPopup.fxml"));
+      Scene homePageScene = new Scene(homePageParent);
+      Stage homeStage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+      homeStage.setScene(homePageScene);
+      homeStage.show();
+      }
   }
 }
