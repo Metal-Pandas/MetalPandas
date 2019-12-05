b29abe5b7c12d1170d9be15bc66148d810ffde1e
Rebbica007 <jrgrunewald5184@eagle.fgcu.edu>
2019-12-04 21:42:29 -0500

    User favourites class added.

--- src/MetalPandasCarApp/UsersFavourites.java
+++ src/MetalPandasCarApp/UsersFavourites.java
@@ -2,4 +2,26 @@ package MetalPandasCarApp;
 
 public class UsersFavourites {
 
+  private String driver;
+  private String startDestination;
+  private String endDestination;
+
+  public UsersFavourites(String driver, String startDestination, String endDestination){
+    this.driver = driver;
+    this.startDestination = startDestination;
+    this.endDestination = endDestination;
+
+  }
+
+  public String getDriver(){ return this.driver; }
+
+  public void setDriver(String driver){ this.driver = driver;}
+
+  public String getStartDestination(){ return this.startDestination; }
+
+  public void setStartDestination(String startDestination){ this.startDestination = startDestination;}
+
+  public String getEndDestination(){ return this.endDestination; }
+
+  public void setEndDestination(String endDestination){ this.endDestination = endDestination;}
 }
