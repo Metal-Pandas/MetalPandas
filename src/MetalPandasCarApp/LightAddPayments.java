ba15538ab6149a0d6cb921f3dc4bf9a7438c393b
Rebbica007 <jrgrunewald5184@eagle.fgcu.edu>
2019-12-05 11:34:07 -0500

    Login doesnt login unless user information is inputted

--- src/MetalPandasCarApp/LightAddPayments.java
+++ src/MetalPandasCarApp/LightAddPayments.java
@@ -1,7 +1,8 @@
 package MetalPandasCarApp;
 
-
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -25,40 +27,83 @@ public class LightAddPayments {
   @FXML public Text paymentType;
   @FXML public ComboBox<String> paymentCombo;
   @FXML public Text cardHolderLabel;
-  @FXML public  TextField cardHolderName;
-  @FXML public  Text cardNumberLabel;
-  @FXML public  TextField cardNumber;
-  @FXML public  Text CCVlabel;
-  @FXML public  TextField paymentCCV;
+  @FXML public TextField cardHolderName;
+  @FXML public Text cardNumberLabel;
+  @FXML public TextField cardNumber;
+  @FXML public Text CCVlabel;
+  @FXML public TextField paymentCCV;
   @FXML public Button paymentConfirm;
   @FXML public Button paymentBack;
   @FXML public Text expirationLabel;
-  @FXML public ComboBox<Integer> expirationMonth;
+  @FXML public ComboBox<String> expirationMonth;
   @FXML public TextField expirationYear;
 
-
-  public void handleBackAction(ActionEvent actionEvent) throws IOException {
-    Parent homePageParent = FXMLLoader.load(getClass().getResource("lightSchedule.fxml"));
-    Scene homePageScene = new Scene(homePageParent);
-    Stage homeStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-    homeStage.setScene(homePageScene);
-    homeStage.show();
-  }
-
-  public void handleConfirmAction(ActionEvent actionEvent) throws IOException {
-    Parent homePageParent = FXMLLoader.load(getClass().getResource("lightPayments.fxml"));
-    Scene homePageScene = new Scene(homePageParent);
-    Stage homeStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-    homeStage.setScene(homePageScene);
-    homeStage.show();
-  }
-
-  public void initialize(){
-    try{
-      paymentCombo.setItems(FXCollections.observableArrayList("Gift Card", "Debit/Credit"));
-      expirationMonth.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
-    } catch (Exception e){
+  public void handleBackAction(ActionEvent actionEvent) {
+    try {
+      Parent homePageParent = FXMLLoader.load(getClass().getResource("lightSchedule.fxml"));
+      Scene homePageScene = new Scene(homePageParent);
+      Stage homeStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+      homeStage.setScene(homePageScene);
+      homeStage.show();
+    } catch (IOException | NullPointerException e) {
       e.printStackTrace();
     }
+  }
+
+  public void handleConfirmAction(ActionEvent actionEvent) {
+    try {
+      if (paymentCombo.getValue().equals("")
+          || cardHolderName.getText().equals("")
+          || cardNumber.getText().equals("")
+          || paymentCCV.getText().equals("")
+          || expirationMonth.getValue().equals("")
+          || expirationYear.getText().equals("")) {
+
+        Alert a = new Alert(Alert.AlertType.NONE);
+        a.setAlertType(Alert.AlertType.WARNING);
+        a.setContentText("Please fill all required fields!");
+        a.show();
+        //System.out.println("The top catch works");
+      } else {
+        addCardPayment();
+        Parent homePageParent = FXMLLoader.load(getClass().getResource("lightPayments.fxml"));
+        Scene homePageScene = new Scene(homePageParent);
+        Stage homeStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        homeStage.setScene(homePageScene);
+        homeStage.show();
+      }
+    } catch (IOException | SQLException | NullPointerException e) {
+      //System.out.println("the bottom exception works!");
+      Alert a = new Alert(Alert.AlertType.NONE);
+      a.setAlertType(Alert.AlertType.WARNING);
+      a.setContentText("Please fill all required fields!");
+      a.show();
+    }
+  }
+
+  public void initialize() {
+    try {
+      paymentCombo.setItems(FXCollections.observableArrayList("Gift Card", "Debit/Credit"));
+      expirationMonth.setItems(
+          FXCollections.observableArrayList(
+              "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void addCardPayment() throws SQLException {
+    String PaymentType = paymentCombo.getValue();
+    String CardHolder = cardHolderName.getText();
+    String CardNumber = cardNumber.getText();
+    String CcvNumber = paymentCCV.getText();
+    String ExpMonth = expirationMonth.getValue();
+    String ExpYear = expirationYear.getText();
+
+    String[] cardPaymentSignUp = {
+        PaymentType, CardHolder, CardNumber, CcvNumber, ExpMonth, ExpYear
+    };
+
+    DatabaseDriver.createCardPaymentInDb(cardPaymentSignUp);
   }
 }
